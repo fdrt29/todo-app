5bf79987da5eba986ed5a0b7ede51a153d7bc2f2
Feodor <fdrt29@ya.ru>
2024-01-10 18:06:46 +0300

    Initial commit

---
+++ pkg/handler/todo_list.go
@@ -0,0 +1,72 @@
+package handler
+
+import (
+	"github.com/fdrt29/todo-app"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+func (h *Handler) createList(c *gin.Context) {
+	userId, err := getUserId(c)
+	if err != nil {
+		return
+	}
+	var input todo.TodoList
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id, err := h.services.TodoList.Create(userId, input)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
+}
+
+type GetAllListsResponse struct {
+	Data []todo.TodoList
+}
+
+func (h *Handler) getAllLists(c *gin.Context) {
+	userId, err := getUserId(c)
+	if err != nil {
+		return
+	}
+
+	lists, err := h.services.TodoList.GetAll(userId)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, GetAllListsResponse{Data: lists})
+}
+func (h *Handler) getListById(c *gin.Context) {
+	userId, err := getUserId(c)
+	if err != nil {
+		return
+	}
+
+	listId, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, "invalid id param")
+		return
+	}
+
+	list, err := h.services.TodoList.GetById(userId, listId)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, list)
+}
+func (h *Handler) updateList(c *gin.Context) {
+
+}
+func (h *Handler) deleteList(c *gin.Context) {
+
+}
